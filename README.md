1e2cb72a2e7fc0383b4428cbc2de8f0b7aa9a1b0
L.Walker <logantwalker95@gmail.com>
2020-08-07 16:28:26 -0400

    1 of 2

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # languageLover
 A script that uses D3.js and the github api to compile a bar graph that shows the percentage of each language used across all of a user's repositories.
+
+
+
+![Title Photo](readme-files/title.png)
+
+## How does it work?
+
+#### linking dependencies
+
+Below are the links you will need in your HTML Head for language-lover.js to function properly
+
+![HTML Head](readme-img/headLinks.png)
+
+you can find the up-to-date links below:
+* https://d3js.org/
+* https://jquery.com/
+
+#### setting up the script 
+
+
---
+++ language-lover.js
@@ -0,0 +1,226 @@
+//enter your username here!
+const username = 'logantwalker';
+
+function getRepoStats() {
+
+    const onReject = (errThrown) => {
+        console.log(errThrown);
+        console.log(errThrown.responseText);
+    }
+
+    $.ajax({
+    
+        url:`https://api.github.com/users/${username}/repos?per_page=100`
+    
+    }).then(function (response) {
+        getLangData(response);
+    
+    }, onReject);
+
+}
+getRepoStats();
+
+var langDataArr=[];
+
+function getLangData(data){
+    const repoCount = data.length;
+    let langEndpoint = [];
+
+    data.forEach(repo =>{
+        let url = repo.languages_url
+        langEndpoint.push(url);
+    });
+
+    const langDataRequest = (urls) =>{
+
+        let stopCondition = urls.length;
+
+        urls.forEach( (urlStr, i) =>{
+
+            const onReject = (errThrown) => {
+                console.log(errThrown);
+                console.log(errThrown.responseText);
+            }
+        
+            $.ajax({
+            
+                url: urlStr
+            
+            }).then(function (response) {
+                
+                dataCollect(response,repoCount);
+                
+            }, onReject);
+        })
+
+    }
+    langDataRequest(langEndpoint);
+    
+    
+
+}
+
+function dataCollect(res,count){
+    langDataArr.push(res);
+    if(langDataArr.length > count -1){
+        compileLangData(langDataArr);
+    }
+    else{
+        return;
+    }
+}
+
+var languagesList = '';
+var langCount = [];
+
+function compileLangData(data){
+    
+    const checkDupes = data =>{
+        data.forEach(obj =>{
+            let keys = Object.keys(obj);
+            
+            keys.forEach(key =>{
+                if(languagesList.includes(key)){
+                    return;
+                }
+                else{
+                    languagesList = languagesList + key + ', ';
+                }
+            })
+        })
+    }
+    checkDupes(data);
+    
+
+    languagesList = languagesList.split(', ');
+    languagesList.pop();
+
+    languagesList.forEach(()=>{
+        langCount.push(0);
+    })
+    console.log(languagesList);
+
+
+
+    const getLangNums = data =>{
+        languagesList.forEach((lang, i) =>{
+            data.forEach(obj =>{
+                let keys = Object.keys(obj);
+                keys.forEach(key =>{
+                    if(key === lang){
+                        langCount[i]+= obj[key];
+                        
+                    }
+                    else{
+                        return;
+                    }
+                })
+            })
+        })
+    }
+
+
+    getLangNums(data);
+
+    const githubStats = {
+        langs: languagesList,
+        vals: langCount
+    };
+
+    dataViz(githubStats);
+    
+}
+
+const finalDataArr= [];
+function dataViz(stats){
+    const langSum = stats.vals.reduce((a,b)=>{
+        return a+b;
+    });
+    
+    const percentArr = stats.vals.map((el)=>{
+        return ((el/langSum)*100).toFixed(2);
+    });
+
+    console.log(percentArr);
+    stats.percentage = percentArr;
+
+    const createDataSet = (stats) =>{
+        let langArr = stats.langs;
+        let langPrct = stats.percentage;
+
+        langArr.forEach((lang,i) =>{
+            finalDataArr.push({
+                language: lang,
+                percent: langPrct[i]
+            })
+        })
+    }
+    createDataSet(stats);
+    console.log(finalDataArr);
+
+    //here is where you can specify the height/width of your graph
+    const margin = 60;
+    var height;
+    var width;
+
+    //this bit of code below handles the mobile responsiveness. 
+    if(window.innerWidth <= 600){
+        //for mobile screens
+        width = (window.innerWidth/1) - 2 * margin; //<-- change the value that "innerWidth" or "innerHeight" is divided by
+        height = (window.innerHeight/1.5) - 2 * margin;// to change the width/height of the graph in relation to window size
+    }
+    else{
+        //for screens greater than 600px wide
+        width = (window.innerWidth/1.5) - 2 * margin;
+        height = (window.innerHeight/1.5) - 2 * margin;
+    }
+
+    console.log(height)
+    console.log(width);
+
+    const svg = d3.select('svg');
+    const chart = svg.append('g')
+    .attr('transform', `translate(${margin}, ${margin})`);
+
+    const yScale = d3.scaleLinear()
+        .range([height, 0])
+        .domain([0, 100]);
+
+    chart.append('g')
+        .call(d3.axisLeft(yScale));
+
+    const xScale = d3.scaleBand()
+        .range([0, width])
+        .domain(finalDataArr.map((s) => s.language))
+        .padding(0.5)
+
+    chart.append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale));
+
+    chart.selectAll()
+        .data(finalDataArr)
+        .enter()
+        .append('rect')
+        .attr('x', (s) => xScale(s.language))
+        .attr('y', (s) => yScale(s.percent))
+        .attr('height', (s) => height - yScale(s.percent))
+        .attr('width', xScale.bandwidth())
+
+    // VERTICAL GRID LINES
+    // chart.append('g')
+    //     .attr('class', 'grid')
+    //     .attr('transform', `translate(0, ${height})`)
+    //     .call(d3.axisBottom()
+    //         .scale(xScale)
+    //         .tickSize(-height, 0, 0)
+    //         .tickFormat(''))
+    
+    //HORIZONTAL GRID LINES
+    chart.append('g')
+        .attr('class', 'grid')
+        .call(d3.axisLeft()
+            .scale(yScale)
+            .tickSize(-width, 0, 0)
+            .tickFormat(''))
+}
